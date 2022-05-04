94ab44e2f0bc3efb0028ec005a8574eb84b45614
CharissaHollister <charissa.hollister@diasorin.com>
2022-05-04 15:26:11 -0500

    add starter code

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Code Quiz - Javascript
+//authored by Charissa Hollister 05/04/2022
+//**************************** */
+
+
+//---------Global variables-----------
+var x = "";
+
+
+
+//-----------Code------------
+
+
+
+
+
+//---------define functions---------
+
+
+
+
+
+
+
+//-------overall function to initiate-------
+
+
+
+
+
+//-----------Get references from HTML------------
+var xxx = document.querySelector("#yyy");
+
+
+
+//---------------Add event listener-----------
+zzz.addEventListener('click', function(event) {
+
+}
